8fa04f0a00d4d241c7e4cbe618795d0dcebf8af2
hailaz <739476267@qq.com>
2026-02-11 09:58:33 +0800

    fix: 修复重复图片问题 (#311)
    * docs: 重构社区图片路径
    
    * fix: 更新社区文档图片路径
    
    * chore: 删除未使用的图片文件
    
    * fix: 避免静态 markdown 图片被重复打包
    
    * articles updates
    
    * add notSkillReference frontmatter for some docs
    
    * docs update
    
    * chore: update en communty excnahe label (#308)
    
    * chore: update
    
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    
    * fix
    
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    
    * fix
    
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    
    * fix
    
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    
    ---------
    
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    
    ---------
    
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>
    Co-authored-by: John <john@johng.cn>
    Co-authored-by: shown <jishiwen.jsw@digital-engine.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,6 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 import type { Config } from '@docusaurus/types';
 import { themes as prismThemes } from 'prism-react-renderer';
 
+const { skipStaticMarkdownImages } = require('./plugins/static-image-exclude-plugin');
+
 function geti18nTitle() {
   switch (process.env.DOCUSAURUS_CURRENT_LOCALE) {
     case 'en':
@@ -82,6 +84,9 @@ const config: Config = {
           // 显示更新时间和作者
           showLastUpdateTime: true,
           showLastUpdateAuthor: true,
+          // 在默认 remark 插件之前运行，跳过 /markdown/ 路径图片的 webpack 处理
+          // 避免 static/markdown 的图片被重复打包到 build/assets/images/
+          beforeDefaultRemarkPlugins: [skipStaticMarkdownImages],
         },
         // Will be passed to @docusaurus/plugin-content-blog (false to disable)
         blog: {},
